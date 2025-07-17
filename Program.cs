0d2101702daed27b5b231841570f8baf7585714b
Vignan Kurme <vignan37@gmail.com>
2025-07-17 18:49:56 +0530

    commands test commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ namespace TestApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, .NET 8!");
+            byte number = 22;
+            Console.WriteLine($"The number is: {number}");
+            //by default real number is double , so add f to make it float
+            float price = 22.08f;
+            Console.WriteLine($"The price is: {price}");
+            bool isWorking = true;
+            Console.WriteLine($"Is it working? {isWorking}");
+            char letter = 'V';
+            Console.WriteLine($"The letter is: {letter}");
+            string name = "Vignan";
+            Console.WriteLine($"my name is {name}");
+
+            //cw shortcut for Console.WriteLine by vs
+            Console.WriteLine();
         }
     }
 }
